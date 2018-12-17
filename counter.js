24aef6100c73e6cd31a372d747d00e590f68570d
FoxWeb <dneprfox@gmail.com>
2018-12-17 22:58:58 +0200

    counter

---
+++ counter.js
@@ -0,0 +1,67 @@
+var counter = (function(){
+
+	
+  
+	var _number;
+
+	var _checkValue = function(y){
+
+		if (y < 0 ) {
+			return false;
+
+		} else {
+			return true;
+		}
+
+	}
+
+
+	var setNumber = function(y){
+		if ( _checkValue(y) ){
+				_number = y;
+		} else {
+				console.log('Недопустимое значение');
+		}
+	}
+
+	var setValue = function(y) {
+
+		   _number = y; 
+	}
+
+	var increaseCounter = function() {
+
+			_number++;
+	}
+
+	var decreaseCounter = function() {
+
+			_number--;
+	}
+
+	return {
+
+		setNumber,
+		increaseCounter,
+		decreaseCounter,
+		printCounter
+
+
+	}
+
+
+}())
+
+counter.Setcounter(-7);
+counter.printCounter();
+counter.increaseCounter();
+
+counter.Setcounter(17);
+counter.increaseCounter();
+counter.increaseCounter();
+counter.printCounter();
+counter.decreaseCounter();
+counter.decreaseCounter();
+counter.printCounter();
+
+
